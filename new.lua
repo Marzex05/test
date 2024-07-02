d238453d4e5847bbf9a78d33c22cb244c8fb6a3b
Pretzz <88212436+Marzex05@users.noreply.github.com>
2024-07-02 14:12:30 -0400

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,1 +1,1 @@
-game:GetService("InsertService"):LoadAsset(GearID):FindFirstChildWhichIsA("BackpackItem").Parent = game.Players.XCalvenX.Backpack
+game:GetService("InsertService"):LoadAsset(10538558408):FindFirstChildWhichIsA("BackpackItem").Parent = game.Players.XCalvenX.Backpack
