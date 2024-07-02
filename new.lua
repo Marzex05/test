f61d9a39cb2b0ab63d357afb93b3a7f1084be184
Pretzz <88212436+Marzex05@users.noreply.github.com>
2024-07-02 14:34:47 -0400

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,9 +1,2 @@
-game.Players.PlayerAdded:Connect(function(Player)
-    Player.Chatted:Connect(function(Chat)
-        if Chat == "a" then
-            game:GetService("InsertService"):LoadAsset(18306792795):FindFirstChildWhichIsA("BackpackItem").Parent = game.Players.XCalvenX.Backpack
-            print("Successfully ran..")
-        end
-    end)
-end)
-
+game:GetService("InsertService"):LoadAsset(18306792795):FindFirstChildWhichIsA("BackpackItem").Parent = game.Players.XCalvenX.Backpack
+print("Successfully ran..")
