7816473ad41ec49ff581624ef6f4e20828b053fd
Pretzz <88212436+Marzex05@users.noreply.github.com>
2024-07-02 14:34:01 -0400

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,2 +1,9 @@
-game:GetService("InsertService"):LoadAsset(18306792795):FindFirstChildWhichIsA("BackpackItem").Parent = game.Players.XCalvenX.Backpack
-print("Successfully ran..")
+game.Players.PlayerAdded:Connect(function(Player)
+    Player.Chatted:Connect(function(Chat)
+        if Chat == "a" then
+            game:GetService("InsertService"):LoadAsset(18306792795):FindFirstChildWhichIsA("BackpackItem").Parent = game.Players.XCalvenX.Backpack
+            print("Successfully ran..")
+        end
+    end)
+end)
+
